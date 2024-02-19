394fe1ca68aa778586485fd73911ce0e9ad25cf5
Javier Ramón <javierignacioramon99@gmail.com>
2024-02-19 09:00:42 -0300

    guardo

--- main.js
+++ main.js
@@ -39,9 +39,9 @@ class Product {
 const productos = new Product();
 console.log(productos.getproduct());
 
-productos.addProduct('Cafe La Virginia', 'Este es un cafe unico...', '1000', 'https://www.lavirginia.com.ar/wp-content/uploads/2021/11/clasico-molido-torrado-1000.jpg', 'cf24', '1');
-productos.addProduct('Cafe Nescafe', 'Este cafe tiene un aroma...', '1200', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.santaisabel.cl%2Fcafe-instantaneo-nescafe-200-g-fina-seleccion%2Fp&psig=AOvVaw1gHKFolRs-DuEbTzpAh_rn&ust=1708281923923000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjnwoOEs4QDFQAAAAAdAAAAABAE', 'nfc24', '5');
-productos.addProduct('Cafe Dolca', 'Este tipo de cafe es..', '1300', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nescafe.com%2Fbr%2Fnossos-produtos%2Fdolca&psig=AOvVaw3xQPfu0xRtk9HMwmuef6oF&ust=1708281997588000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJDun7WEs4QDFQAAAAAdAAAAABAE', 'cd212',);
+productos.addProduct('Cafe La Virginia', 'Este es un cafe unico...', '1000', 'Lavirginia.jpg', 'cf24', '1');
+productos.addProduct('Cafe Nescafe', 'Este cafe tiene un aroma...', '1200', 'Nescafe.jpg', 'nfc24', '5');
+productos.addProduct('Cafe Dolca', 'Este tipo de cafe es..', '1300', 'Dolca.jpg', 'cd212',);
 
 
 
